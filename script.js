26778b98d519848022a8b0244291fcc75b4d0b29
Joshua Bliek <jbliek213@yahoo.com>
2023-12-12 17:17:14 -0800

    more features and polishing

--- script.js
+++ script.js
@@ -7,3 +7,24 @@ const toggleFlip = (elem) => {
 const linkClick = (event) => {
     event.stopPropagation();
 }
+
+const trigger = document.getElementById('head');
+const cursorShadow = document.querySelector('.cursor-shadow');
+
+document.addEventListener('mousemove', function (event) {
+    const x = event.clientX;
+    const y = event.clientY;
+
+    cursorShadow.style.transform = `translate(${x - 70}px, ${y - 250}px)`;
+});
+
+trigger.addEventListener('mouseenter', function () {
+    cursorShadow.style.opacity = 1;
+});
+
+// nav.addEventListener('mouseleave', function () {
+//     setTimeout(() => {
+        
+//     }, 1s);
+//     cursorShadow.style.opacity = 0;
+// });
